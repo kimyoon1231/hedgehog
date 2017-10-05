ee5331857ded8729921ebcf5a01c0eea57a68f4f
kimyoon1231 <32539160+kimyoon1231@users.noreply.github.com>
2017-10-05 21:01:12 +0900

    Add files via upload

---
+++ p135_Q7.java
@@ -0,0 +1,38 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class p135_Q7 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int menu;
+		int money = 0;
+		while (true) {
+
+			System.out.println("-----------------------");
+			System.out.println("1.예금|2.출금|3.잔고|4.종료");
+			System.out.println("-----------------------");
+			System.out.println("선택>");
+			menu = in.nextInt();
+
+			switch (menu) {
+			case 1:
+				System.out.println("예금액>");
+				money += in.nextInt();
+				break;
+			case 2:
+				System.out.println("출금액>");
+				money -= in.nextInt();
+				break;
+			case 3:
+				System.out.println("잔고>" + money);
+				break;
+			case 4:
+				System.out.println("프로그램 종료");
+				return;
+			}
+		}
+	}
+}
