9db912bb9cbfc70d9b1e3e3ff1231be9f95aea2d
kimyoon1231 <32539160+kimyoon1231@users.noreply.github.com>
2017-10-07 13:31:11 +0900

    Add files via upload

---
+++ smart/Q4_Person_2.java
@@ -0,0 +1,53 @@
+public class Q4_Person_2 {
+	private int membership_number;
+	private String name;
+	private String phone_number;
+	private String address;
+
+	public Q4_Person_2(int membership_number, String name, String phone_number, String address) {
+		super();
+		this.membership_number = membership_number;
+		this.name = name;
+		this.phone_number = phone_number;
+		this.address = address;
+	}
+
+	public Q4_Person_2() {
+	}
+
+	public int getMembership_number() {
+		return membership_number;
+	}
+
+	public void setMembership_number(int membership_number) {
+		this.membership_number = membership_number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void print() {
+		System.out.printf("번호: %-5d이름: %-15s전화번호: %-20s주소: %-30s\n", membership_number, name, phone_number, address);
+	}
+}
