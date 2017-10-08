58623b3614241ca1cbcebd392cb99666e19201b0
kimyoon1231 <32539160+kimyoon1231@users.noreply.github.com>
2017-10-09 00:16:53 +0900

    Add files via upload

---
+++ standard/S6_2_SutdaCard.java
@@ -0,0 +1,35 @@
+package javaPractice;
+// standard 6-2
+public class S6_2_SutdaCard {
+	int num;
+	boolean isKwang;
+
+	public S6_2_SutdaCard() {
+		this(1, true); // default constructor
+	}
+
+	public S6_2_SutdaCard(int num, boolean isKwang) {
+		super();
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+
+	public String info() {
+		return num + (isKwang ? "K" : "");
+		// return num + if(isKwang == true ) {"K"} : X !!
+	}
+}
+
+
+//
+//public class SutdaCardEx {
+//
+//	public static void main(String[] args) {
+//
+//		SutdaCardEx c1 = new SutdaCardEx(3, false);
+//		SutdaCardEx c2 = new SutdaCardEx();
+//		
+//		System.out.println(c1.info());
+//		System.out.println(c2.info());
+//	}
+//}
